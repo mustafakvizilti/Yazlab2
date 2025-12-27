1615c4a3de37da498f13184f88890251d0122c1d
mustafakvizilti <kucukviziltimustafa@gmail.com>
2025-12-27 21:27:27 +0300

    tasarım

---
+++ Yazlab2WinForms/Yazlab2WinForms/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Yazlab2WinForms
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtNodeName = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNodeName
+            // 
+            this.txtNodeName.Location = new System.Drawing.Point(0, 0);
+            this.txtNodeName.Name = "txtNodeName";
+            this.txtNodeName.Size = new System.Drawing.Size(100, 22);
+            this.txtNodeName.TabIndex = 0;
+            this.txtNodeName.Text = "Düğüm adı";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(0, 28);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(100, 22);
+            this.txtSource.TabIndex = 1;
+            this.txtSource.Text = "Kaynak";
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Location = new System.Drawing.Point(0, 56);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(100, 22);
+            this.txtTarget.TabIndex = 2;
+            this.txtTarget.Text = "Hedef";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Düğüm Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnAddNode_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Bağlantı Ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnAddEdge_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(-3, 139);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(105, 16);
+            this.lblInfo.TabIndex = 6;
+            this.lblInfo.Text = "Bilgi bekleniyor..";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(478, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 378);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.picGraph_Paint);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.txtNodeName);
+            this.Name = "Form1";
+            this.Text = "Graf Çizim Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtNodeName;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Yazlab2WinForms/Yazlab2WinForms/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yazlab2WinForms
+{
+    public partial class Form1 : Form
+    {
+        List<string> nodes = new List<string>();
+        List<(string, string)> edges = new List<(string, string)>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblInfo.Text = "Graf uygulaması hazır!";
+        }
+
+        private void txtNodeName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddNode_Click(object sender, EventArgs e)
+        {
+            string node = txtNodeName.Text.Trim();
+
+            if (string.IsNullOrEmpty(node))
+            {
+                lblInfo.Text = "Lütfen bir düğüm adı girin!";
+                return;
+            }
+
+            if (nodes.Contains(node))
+            {
+                lblInfo.Text = "Bu düğüm zaten var!";
+                return;
+            }
+
+            nodes.Add(node);
+            lblInfo.Text = $"Düğüm eklendi: {node}";
+            txtNodeName.Clear();
+        }
+
+
+        private void txtSource_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTarget_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picGraph_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddEdge_Click(object sender, EventArgs e)
+        {
+            string s = txtSource.Text.Trim();
+            string t = txtTarget.Text.Trim();
+
+            if (!nodes.Contains(s) || !nodes.Contains(t))
+            {
+                lblInfo.Text = "Bağlantı için her iki düğüm de var olmalı!";
+                return;
+            }
+
+            edges.Add((s, t));
+            lblInfo.Text = $"Kenar eklendi: {s} → {t}";
+            txtSource.Clear();
+            txtTarget.Clear();
+        }
+
+private void picGraph_Paint(object sender, PaintEventArgs e)
+{
+    Graphics g = e.Graphics;
+    int x = 20, y = 20;
+
+    foreach (var node in nodes)
+    {
+        g.FillEllipse(Brushes.LightBlue, x, y, 50, 50);
+        g.DrawEllipse(Pens.Black, x, y, 50, 50);
+        g.DrawString(node, this.Font, Brushes.Black, x+15, y+15);
+        y += 70;
+    }
+}
+
+    }
+}
